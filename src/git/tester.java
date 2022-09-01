b39869c3675e915047258c65856c36a7b08218dc
Asher <asher.rossen@gmail.com>
2022-09-01 15:01:30 -0700

    file stuff in tester

--- src/git/tester.java
+++ src/git/tester.java
@@ -1,9 +1,11 @@
 package git;
 
+import java.io.IOException;
+
 public class tester {
 
-	public static void main(String[] args) {
-		Blob b = new Blob ("a");
+	public static void main(String[] args) throws IOException {
+		Blob b = new Blob (".\\Git Prereq\\something.txt");
 		System.out.println(b.encryptThisString("abcdefg"));
 
 	}
