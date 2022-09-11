4d52cce7b0f45da4c85a01986cc6b0caebdf1681
Asher <asher.rossen@gmail.com>
2022-09-11 12:57:21 -0700

    testing index

--- src/git/tester.java
+++ src/git/tester.java
@@ -7,6 +7,7 @@ public class tester {
 	public static void main(String[] args) throws IOException {
 		//Blob b = new Blob ("/Users/asher/eclipse-workspace/Git Prereq/something.txt");
 //		System.out.println(b.encryptThisString("abcdefg"));
+		Index i = new Index();
 
 	}
 
