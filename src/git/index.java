5bbb2510217effa3e80038f1f7b53c2bf9fa226c
Asher <asher.rossen@gmail.com>
2022-09-11 14:57:00 -0700

    remove method

--- src/git/index.java
+++ src/git/index.java
@@ -1,6 +1,9 @@
 package git;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -25,12 +28,22 @@ public class Index {
 	
 	public void add(String fn) throws IOException {
 		Blob blobby = new Blob (fn);
-		if (!hm.containsValue(blobby.getName())) {
+		if (!hm.containsKey(fn.substring(42))) {
 			hm.put(fn.substring(42), blobby.getName());
 			FileWriter fw = new FileWriter("/Users/asher/eclipse-workspace/Git Prereq/index.txt");
-			fw.write(fn.substring(42)+" : "+blobby.getName());
+			fw.write(fn.substring(42)+" : "+blobby.getName()+"\n");
 			fw.close();
 		}
+	}
+	
+	public void remove(String fn) throws IOException {
+		if (hm.containsKey(fn.substring(42))) {
+			String str = Blob.encryptThisString(fn);
+			hm.remove(fn.substring(42), str);
+			BufferedReader br = new BufferedReader(new FileReader ("/Users/asher/eclipse-workspace/Git Prereq/index.txt"));
+			
+		}
+		
 	}
 	
 	private void makeFile(String s) throws IOException {
