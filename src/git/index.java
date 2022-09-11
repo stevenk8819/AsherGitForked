d232cd0178b029277cc8d78e8fef5ebbd95a6e5c
Asher <asher.rossen@gmail.com>
2022-09-11 12:55:57 -0700

    created index

---
+++ src/git/index.java
@@ -0,0 +1,16 @@
+package git;
+
+import java.io.File;
+
+public class index {
+	//private boolean bool = false;
+	private File index;
+	
+	public index () {
+//		File file = new File("/Users/asher/eclipse-workspace/Git Prereq");
+//		//creating the directory
+//		bool = file.mkdir();
+		index = new File ("/Users/asher/eclipse-workspace/Git Prereq/objects/index.txt");
+		
+	}
+}
