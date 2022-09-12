4ff0859280801d2151bc7467635432b0694b9787
Asher <asher.rossen@gmail.com>
2022-09-12 00:09:54 -0700

    deleting text from index

--- src/git/index.java
+++ src/git/index.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -42,6 +43,20 @@ public class Index {
 			String str = Blob.encryptThisString(fn);
 			hm.remove(fn.substring(42), str);
 			BufferedReader br = new BufferedReader(new FileReader ("/Users/asher/eclipse-workspace/Git Prereq/index.txt"));
+			PrintWriter pw = new PrintWriter("/Users/asher/eclipse-workspace/Git Prereq/temp.txt");
+			while (br.ready()) {
+				String read = br.readLine();
+				if (read.indexOf(fn.substring(42))<0) {
+					pw.print (read);
+				}
+			}
+			br.close();
+			pw.close();
+			File ind = new File ("/Users/asher/eclipse-workspace/Git Prereq/index.txt");
+			File temp = new File ("/Users/asher/eclipse-workspace/Git Prereq/temp.txt");
+			ind = temp;
+			temp.delete();
+			
 			File file = new File (fn);
 			file.delete();
 		}
